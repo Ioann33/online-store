45affd5afcfcae9a1a5e515ab04d5de8418b7494
Alexsey Molozhenko <jukov20150@gmail.com>
2022-05-08 03:41:43 +0300

    add  indexController __construct() , index()

--- vendor/controllers/IndexController.php
+++ vendor/controllers/IndexController.php
@@ -6,9 +6,14 @@ use core\View;
 class IndexController extends \core\AbstractController
 {
 
+    public function __construct()
+    {
+        parent::__construct('front');
+    }
+
     public function index()
     {
-        $this ->view->render('front');
+        $this ->view->render('index_index_view');
     }
 
 
