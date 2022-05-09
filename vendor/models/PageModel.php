2cf73df8b9a1d81792d136f3f865ed8e498296cd
Alexsey Molozhenko <jukov20150@gmail.com>
2022-05-09 16:13:47 +0300

    PageModel update method

--- vendor/models/PageModel.php
+++ vendor/models/PageModel.php
@@ -9,11 +9,11 @@ class pageModel extends AbstractModel
 
     /**
      * getting data from (pages) table by content name
-     * @param $namePage
-     * @return mixed
+     * @param $namePage ('home','deliverypay','contacts')
+     * @return array
      * @throws \Exception
      */
-    public function page($namePage){
+    public function showPage($namePage){
         $sql = "SELECT * FROM pages WHERE NAME LIKE '{$namePage}'; ";
 
         $result = $this->db->query($sql);
@@ -21,6 +21,18 @@ class pageModel extends AbstractModel
             throw new \Exception($this->db->errno);
         }
         return $result->fetch_all(MYSQLI_ASSOC) ;
+    }
+
+    /**
+     * update data in (pages) table by ID and NAME property
+     * @param $id
+     * @param $namePage ('home','deliverypay','contacts')
+     * @param $text
+     * @return bool
+     */
+    public function updatePage($id,$namePage,$text){
+        $sql="UPDATE pages UpdateSET text ='{$text}' WHERE ID = '{$id}' AND NAME = '{$namePage}';";
+        return $this->db->query($sql);
     }
 
 }
