49b5839502d8ed186c8d4a0ad64ae513babe0b94
Ioann33 <ivan.karmalyk2017@gmail.com>
2022-05-09 21:29:16 +0300

    remake index

--- vendor/controllers/IndexController.php
+++ vendor/controllers/IndexController.php
@@ -3,9 +3,9 @@
 namespace controllers;
 
 use core\View;
+
 class IndexController extends \core\AbstractController
 {
-
     public function __construct()
     {
         parent::__construct('front');
@@ -13,8 +13,22 @@ class IndexController extends \core\AbstractController
 
     public function index()
     {
+        //TODO remake model class and connect their
+        $page = $this->pageModel->showPage();// Home page content
         $this ->view->render('index_index_view');
     }
 
+    public function contacts(){
+        //TODO remake model class and connect their
+        $page = $this->pageModel->showPage();// Contacts page content
+        $page.= ' contacts';
+        $this ->view->render('index_contacts_view', ['page'=>$page]);
+    }
 
+    public function deliveryPay(){
+        //TODO remake model class and connect their
+        $page = $this->pageModel->showPage();// deliveryPay page content
+        $page.= ' deliveryPay';
+        $this ->view->render('index_deliverypay_view', ['page'=>$page]);
+    }
 }
