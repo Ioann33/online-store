704faaff70d35310a702e5fa7904e6ed57236a60
Ioann33 <ivan.karmalyk2017@gmail.com>
2022-05-10 19:34:07 +0300

    changed simulate model on real

--- vendor/models/categoryModel.php
+++ vendor/models/categoryModel.php
@@ -1,12 +1,72 @@
 <?php
 
-
 namespace models;
 
+use core\AbstractModel;
 
-class categoryModel
+class CategoryModel extends AbstractModel
 {
+
+    /**
+     * get all categories
+     * @return array
+     * @throws \Exception
+     */
     public function showCategories(){
-        return "one category </br>two category </br>three category </br></br>four category </br>";
+        $sql = "SELECT * FROM categories ;";
+        $result = $this->db->query($sql);
+        if($this->db->errno !==0){
+            throw new \Exception($this->db->errno);
+        }
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
+
+    /**
+     *  get products of a specific category
+     * @param $categoryId
+     * @return array
+     * @throws \Exception
+     */
+    public function showCategory($categoryId){
+        $sql="SELECT * FROM products WHERE category_id = '{$categoryId}';";
+        $result = $this->db->query($sql);
+        if($this->db->errno !==0){
+            throw new \Exception($this->db->errno);
+        }
+        return $result->fetch_all(MYSQLI_ASSOC);
+
+    }
+
+    /**
+     * save new category
+     * @param $name
+     * @return bool
+     */
+    public function saveCategory($name){
+        $sql="INSERT INTO categories (name) VALUES('{$name}'); ";
+
+       return $this->db->query($sql);
+    }
+
+    /**
+     * update Category(name)
+     * @param $id
+     * @param $updateName
+     * @return bool
+     */
+    public function updateCategory($id,$updateName){
+        $sql="UPDATE categories SET name = '{$updateName}' WHERE id = '{$id}';";
+        return $this->db->query($sql);
+    }
+
+    /**
+     * delete Category
+     * @param $id
+     * @return bool
+     */
+    public function deleteCategory($id){
+        $sql="DELETE  FROM categories WHERE id = '{$id}';";
+        return $this->db->query($sql);
+    }
+
 }
--- vendor/models/pageModel.php
+++ vendor/models/pageModel.php
@@ -1,16 +1,38 @@
 <?php
 
-
 namespace models;
 
+use core\AbstractModel;
 
-class pageModel
+class pageModel extends AbstractModel
 {
-    public function showPage(){
-        return 'show page text';
+
+    /**
+     * getting data from (pages) table by content name
+     * @param $namePage ('home','deliverypay','contacts')
+     * @return array
+     * @throws \Exception
+     */
+    public function showPage($namePage){
+        $sql = "SELECT * FROM pages WHERE NAME LIKE '{$namePage}'; ";
+
+        $result = $this->db->query($sql);
+        if($this->db->errno !==0){
+            throw new \Exception($this->db->errno);
+        }
+        return $result->fetch_all(MYSQLI_ASSOC) ;
     }
 
-    public function updatePage(){
-        return 'update page';
+    /**
+     * update data in (pages) table by ID and NAME property
+     * @param $id
+     * @param $namePage ('home','deliverypay','contacts')
+     * @param $text
+     * @return bool
+     */
+    public function updatePage($id,$namePage,$text){
+        $sql="UPDATE pages UpdateSET text ='{$text}' WHERE ID = '{$id}' AND NAME = '{$namePage}';";
+        return $this->db->query($sql);
     }
+
 }
