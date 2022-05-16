bc2129207621dc4632562b923f62e84de3503a1a
YoungDobrodum <yung.dobrodum@gmail.com>
2022-05-16 20:19:54 +0300

    AdminDeliveryPayController index
    Signed-off-by: YoungDobrodum <yung.dobrodum@gmail.com>

---
+++ vendor/views/admin/admindeliverypay_index.view.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+    <span class="navbar-text">
+      Admin front Delivery Pay
+    </span>
+    </div>
+</nav>
+<p class="lead">
+    Some paragraph about payment and delivery
+</p>
+
+
